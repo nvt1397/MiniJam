2d85b1cafb78010679b5db81371db296bbc2366a
Sasuke201 <linh.huynh.dev.99@gmail.com>
2020-03-20 20:36:20 +0700

    enemy move

--- MiniJam50/Assets/Scripts/FollowBehaviour.cs
+++ MiniJam50/Assets/Scripts/FollowBehaviour.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class FollowBehaviour : StateMachineBehaviour
 {
     private Transform playerPos;
+    private Transform enemyPos;
+    private Vector3 oldEnemyPos;
+    public string enemyName;
     public float speed;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         playerPos = GameObject.FindGameObjectWithTag("Player1").transform;
+        enemyPos = GameObject.FindGameObjectWithTag(enemyName).transform;
+        oldEnemyPos = enemyPos.transform.position;
     }
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.transform.position = Vector2.MoveTowards(animator.transform.position, playerPos.position, speed * Time.deltaTime);
+        facePlayer1();
+        float rFollow = Mathf.Sqrt(Mathf.Pow((playerPos.position.x - enemyPos.position.x), 2f) + Mathf.Pow((playerPos.position.y - enemyPos.position.y), 2f));
+        if (rFollow > 2f)
+        {
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, oldEnemyPos, speed * Time.deltaTime);
+            animator.SetBool("isFollowing", false);
+        }
     }
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
 
     }
-
+    public void facePlayer1()
+    {
+        Vector2 direction = new Vector2(playerPos.position.x - enemyPos.position.x, playerPos.position.y - enemyPos.position.y);
+        enemyPos.transform.up = direction;
+    }
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
--- MiniJam50/Assets/Scripts/IdleBehaviour.cs
+++ MiniJam50/Assets/Scripts/IdleBehaviour.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class IdleBehaviour : StateMachineBehaviour
 {
+    private Transform playerPos;
+    private Transform enemyPos;
+    public string enemyName;
+    public float r;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        playerPos = GameObject.FindGameObjectWithTag("Player1").transform;
+        enemyPos = GameObject.FindGameObjectWithTag(enemyName).transform;
     }
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float rFollow = Mathf.Sqrt(Mathf.Pow((playerPos.position.x - enemyPos.position.x), 2f) + Mathf.Pow((playerPos.position.y - enemyPos.position.y), 2f));
+        if (rFollow <= r)
         {
             animator.SetBool("isFollowing", true);
         }
+
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
